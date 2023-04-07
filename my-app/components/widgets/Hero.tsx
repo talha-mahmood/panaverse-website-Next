c34b6dde39ad43395ff305dd8ec526d7c3c41008
talha-mahmood <talhamahmood1261@gmail.com>
2023-04-07 08:01:56 -0700

    2nd commit

--- my-app/components/widgets/Hero.tsx
+++ my-app/components/widgets/Hero.tsx
@@ -4,14 +4,14 @@ import happy_student from '@/public/happy_student.png'
 import Image from 'next/image'
 const Hero = () => {
   return (
-    <section className='bg-red-200'>
+    <section >
     <Wrapper>
-      <div className='flex'>
+      <div className='flex flex-col md:flex-row items-center'>
         {/* Left side */}
         <div className='flex-1'>
-          <h4>Presidential Initiative for Artificial Intelligence and Computing(PIAIC)</h4>
-          <h1>Certified Web 3.0 and Metaverse Developer</h1>
-          <p> As you learn from panaverse you are getting ready for next generartion of internet
+          <h4 className='text-teal-800 font-semibold text-lg'>Presidential Initiative for Artificial Intelligence and Computing(PIAIC)</h4>
+          <h1 className='text-6xl font-bold text-gray-900 md:text-red-400 lg:text-blue-700'>Certified Web 3.0 and Metaverse Developer</h1>
+          <p className='mt-6 text-lg text-slate-600'> As you learn from panaverse you are getting ready for next generartion of internet
             Consolidating Web 3.0 and Metaverse, Artificial Intelligence, Blockchain, Internet of Things
           </p>
           <button> Learn more</button>
