506110651d00eebdccaa750fe04d71acc859a1a4
talha-mahmood <talhamahmood1261@gmail.com>
2023-04-12 04:29:07 -0700

    10th commit

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -9,12 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <header>
-        <title>Panaverse</title>
-   
-        <link rel="shortcut icon" href="@/app/favicon.ico" sizes='215x215'/>
-
-      </header> */}
+     
       <body>
         <Header/> 
         {children}
